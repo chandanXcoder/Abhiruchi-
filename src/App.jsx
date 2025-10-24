2ba5bdf5f1909e088c8702040b30b8e6b8293b33
chandan-xcoder <chaandanverma@gmail.com>
2025-10-24 13:14:13 +0530

    event component remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Hero from './component/Hero'
 import About from './component/About'
 import Navbar from './component/Navbar'
-import Event from './component/Event'
 // import Gallery from './component/Gallary'
 import Contact from './component/Contact'
 import Footer from './component/Footer'
